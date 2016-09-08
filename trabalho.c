26da395e58d6324c81489ad55c1f9f5b5190dfc2
Thiago <tberaldin@gmail.com>
2016-09-08 19:05:36 -0300

    terminando a leitura

--- trabalho.c
+++ trabalho.c
@@ -23,26 +23,36 @@ int generateRandomDiagonal( unsigned int N, unsigned int k, unsigned int kMax, d
 
   return (0);
 
-
-int main (int argc, char *argv[]) {
+int main (int argc, char *argv[])
+{
 	int n = k = i = 0;
 	double t = 0.0;
-	char output[];
-	if (argc == 9) { //todos os parametros
- 	n = atoi(argv[1]);
-	k = atoi(argv[2]);
-	i = atoi(argv[4]);
-	t = atof(argv[6]);
-	strcpy (output, argv[8]);
+	char *output;
+	if (argc == 9)
+	{ //todos os parametros
+ 		n = atoi(argv[1]);
+		k = atoi(argv[2]);
+		i = atoi(argv[4]);
+		t = atof(argv[6]);
+		strcpy (output, argv[8]);
 	}
-	else if (argc == 7) {
-		if
+	else if (argc == 7)
+	{
+		if (argv[3] == '-i')
+			 i = argv[4]
+		else t = argv[4];
+		strcpy(output, argv[6];
 	}
-	
-	if 
-	double *x = malloc (100 * sizeof (double));
-	for (int i=0; i<100; ++i)
-		x[i] = 0.0;
-	generateRandomDiagonal (100, 0, 5, x);
+	else
+	{
+		strcpy(output, argv[4];
+	}
+
+	double ** m = malloc ((k+1)*sizeof(double));
+	for (int i=0; i<k; ++i)
+	{
+		m[i] = malloc(n*sizeof(double));
+		generateRandomDiagonal (n, i, k, m[i]);
+	}
 	return 0;
 }
