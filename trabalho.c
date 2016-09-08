c6d439865e5fcd47061a410d620396f38cf1e7f1
Luis <luis@netbook.(none)>
2016-09-08 16:38:52 -0300

    criei o arquivo

---
+++ trabalho.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/***********************
+ * N: tamanho do sistema linear
+ * k: numero da diagonal, 0 = diagonal principal, 1 = acima/abaixo da diagonal, 2 = ...
+ * kMax: numero de bandas do sistema linear
+ * diag: vetor para armazenar os valores da diagonal. Deve ser alocado por quem chama a função.
+ ***********************/
+int generateRandomDiagonal( unsigned int N, unsigned int k, unsigned int kMax, double *diag )
+{
+  if ( !diag || N < 3 || kMax > N/2 || k < 0 || k > kMax )
+    return (-1);
+
+  /* garante valor dominante para diagonal principal */
+  double fator = (k == 0) ? ((double)(kMax-1)) : (0.0);
+
+  double invRandMax = 1.0 / (double)RAND_MAX;
+
+  for (int i=0; i < N-k; ++i)
+  {
+    diag[i] = fator + (double)rand() * invRandMax;
+  }
+
+  return (0);
+}
